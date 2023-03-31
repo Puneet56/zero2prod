84771051bf9408b57fb82734f9aa11a0f7915f90
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-03-31 12:50:39 +0530

    added database connection and tests to check record in db

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::startup::run;
+
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let configuration = get_configuration().expect("Failed to read configuration");
 
-    run(listner)?.await
+    let connection_string = configuration.database.connection_string();
+
+    let connection_pool = PgPool::connect(&connection_string)
+        .await
+        .expect("Failed to connect to database");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listner = TcpListener::bind(address).expect("Failed to bind address");
+
+    run(listner, connection_pool)?.await
 }
