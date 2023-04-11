19e0a1fdf5d785e1b255a449e6dfe2c2b5d6dabd
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-04-11 21:35:42 +0530

    Revert "configuration wip"
    This reverts commit a6d97da60fa4a0b8bc4d0c67b0a8f4ff476c7d01.

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,11 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
             .expect("Failed to connect to Postgres.");
 
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
+    let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
 
