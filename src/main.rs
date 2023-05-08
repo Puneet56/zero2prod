c0f9272d828bcbf68548bb248b4d2372284eb8a2
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-05-08 17:46:26 +0530

    Wrote first integration test. Moved the server code to a lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+
+    run(listner)?.await
 }
